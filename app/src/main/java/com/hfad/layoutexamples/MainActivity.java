9ed666310821cf54315014cdb61be9e86fed2741
Niels Wadsholt <eaaniwa@students.eaaa.dk>
2018-09-17 00:21:55 +0200

    Initial commit

---
+++ app/src/main/java/com/hfad/layoutexamples/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hfad.layoutexamples;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSendMessage(View view) {
+    }
+}
